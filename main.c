4ea589c1ba6961617215e81230acbb2f52c9f549
медвежий4 <mihailbelov932@gmail.com>
2026-09-19 12:15:35 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+    Студент: Белов Михаил Александрович
+    Группа: ПИ 1-1
+    Назнаение: "Конвертер величин"
+*/
+int main(void)
+{
+    double value, result;
+    int command;
+    
+    printf("Введите два числа:\n");
+    scanf("%lf", &value);
+    printf("Введите два числа:\n");
+    printf("Введите два числа:\n");
+    printf("Введите два числа:\n");
+    scanf("%lf", &value);
+}
