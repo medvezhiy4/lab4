f938044a162ce7dc5f1758b310804db889bed842
медвежий4 <mihailbelov932@gmail.com>
2026-09-19 13:17:37 +0300

    main.c

--- main.c
+++ main.c
@@ -11,8 +11,30 @@ int main(void)
     
     printf("Введите два числа:\n");
     scanf("%lf", &value);
-    printf("Введите два числа:\n");
-    printf("Введите два числа:\n");
-    printf("Введите два числа:\n");
-    scanf("%lf", &value);
+    printf("1 - м в см\n2 кг в гр\n");
+    printf("3 - гр.ц. в гр.ф.\n4 - часы в мин\n");
+    printf("Выберите операцию: ");
+    scanf("%xd", &command);
+    
+    switch(command)
+    {
+    case 1:
+       result = value * 100;
+        printf("Результат: %.2f\n", result);
+        break;
+    case 2:
+        result = value * 1000;
+        printf("Результат: %.2f\n", result);
+        break;
+    case 3:
+        result = value * 9 / 5 + 32;
+        printf("Результат: %.2f\n", result);
+        break;
+    case 4:
+        result = value * 60;
+        printf("Результат: %.2f\n", result);
+        break;
+    }
+    
+    return 0;
 }
